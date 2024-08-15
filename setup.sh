42ea740de617e062a9e042e9b66bb0a668dd7feb
Rastislav Uhliar <void@mnau.local>
2024-08-15 18:55:53 +0200

    Added alias

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
-ln -s $PWD/.zprofile ~/.zprofile
-ln -s $PWD/.zshrc ~/.zshrc
+#ohmyzsh theme name kafeitu
+#ln -s $PWD/.zprofile ~/.zprofile
+#ln -s $PWD/.zshrc ~/.zshrc
 ln -s $PWD/.vimrc ~/.vimrc
 ln -s $PWD/.hushlogin ~/.hushlogin
 ln -s $PWD/skhdrc ~/.config/skhd/skhdrc
 ln -s $PWD/openiterm2.sh ~/.config/skhd/openiterm2.sh
 ln -s $PWD/yabairc ~/.config/yabai/yabairc
-
+echo "alias ls='ls -Ga'" >> ~/.zshrc
