20c5e58056b759ae3730884efdc0578bb690f34b
Rastislav Uhliar <void@mnau.local>
2024-08-15 18:24:11 +0200

    Symlink creator

---
+++ setup.sh
@@ -0,0 +1,8 @@
+ln -s $PWD/.zprofile ~/.zprofile
+ln -s $PWD/.zshrc ~/.zshrc
+ln -s $PWD/.vimrc ~/.vimrc
+ln -s $PWD/.hushlogin ~/.hushlogin
+ln -s $PWD/skhdrc ~/.config/skhd/skhdrc
+ln -s $PWD/openiterm2.sh ~/.config/skhd/openiterm2.sh
+ln -s $PWD/yabairc ~/.config/yabai/yabairc
+
