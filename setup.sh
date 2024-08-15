d3d49269b3c5bc6bbbdb77cb11bc5a576d853892
Rastislav Uhliar <void@Rastislavs-MacBook-Air.local>
2024-08-15 21:25:11 +0200

    Fixed dir doesnt exist

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 #ohmyzsh theme name kafeitu
 #ln -s $PWD/.zprofile ~/.zprofile
 #ln -s $PWD/.zshrc ~/.zshrc
+mkdir ~/.config
+mkdir ~/.config/yabai
+mkdir ~/.config/skhd
 ln -s $PWD/.vimrc ~/.vimrc
 ln -s $PWD/.hushlogin ~/.hushlogin
 ln -s $PWD/skhdrc ~/.config/skhd/skhdrc
