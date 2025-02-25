d2b80df7e46575ba694265af16894275a8288772
Rastislav Uhliar <void@2532703090.wifi.kn.vutbr.cz>
2025-02-25 22:48:36 +0100

    Changed gruvbox theme to black color

--- nvim/lua/plugins/gruvbox.lua
+++ nvim/lua/plugins/gruvbox.lua
@@ -1,4 +1,21 @@
+-- return {
+--   "ellisonleao/gruvbox.nvim", priority = 1000 , config = true
+-- }
+--
 return {
-  "ellisonleao/gruvbox.nvim", priority = 1000 , config = true
+  "ellisonleao/gruvbox.nvim",
+  priority = 1000,
+  config = function()
+    require("gruvbox").setup({
+      contrast = "hard",  -- make background darker (options: soft/medium/hard)
+      transparent_mode = false,  -- disable transparency
+      overrides = {
+        Normal = { bg = "#000000" },  -- pure black background
+        NonText = { bg = "#000000" }, -- pure black for non-text areas
+        LineNr = { bg = "#000000" },  -- line numbers background
+      }
+    })
+    vim.o.background = "dark"
+    vim.cmd([[colorscheme gruvbox]])
+  end
 }
-
